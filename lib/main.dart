8a96a35fe37154e3c49e71e26a7d2af91e0de5f1
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-01-22 15:16:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:poshrob/HomeBase.dart';
 import 'package:poshrob/accounts/LoginPage.dart';
 import 'package:poshrob/fragments/HomePage.dart';
+import 'package:flutter/services.dart';
+import 'package:poshrob/shared_pref.dart';
 
-void main() {
-  runApp(MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool userLoggedIn=false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async{
+    await HelperFunctions.getUserLoggedIn().then((value){
+      setState(() {
+        userLoggedIn=value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      title: 'PoshRobe',
+      home: userLoggedIn? HomeBase(): LoginPage(),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(
-        const Duration(seconds: 3),
-        () => Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
-            ));
-
-    return Scaffold(
-        body: Center(
-      child: Image(
-        image: AssetImage('images/app_logo.png'),
-        width: 200,
-        height: 150,
-      ),
-    ));
-  }
-}
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     Future.delayed(
+//         const Duration(seconds: 3),
+//         () => Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => LoginPage()),
+//             ));
+//
+//     return Scaffold(
+//         body: Center(
+//       child: Image(
+//         image: AssetImage('images/app_logo.png'),
+//         width: 200,
+//         height: 150,
+//       ),
+//     ));
+//   }
+// }
